210512510cd8e5f60aca41a48cfd659a8721a90a
Surendra <sky958447@gmail.com>
2025-02-15 12:37:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//your JS code here. If required.
-let status = document.getElementById("status");
-let enterBtn = document.getElementById("enterBtn");
-
-let h1Element = document.createElement("h1");
-h1Element.textContent = "Entered Metaverse";
-enterBtn.addEventListener('click',function(){
-	status.parentNode.replaceChild(h1Element,status);
-	
-})
+function enter() {
+    document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
+}
